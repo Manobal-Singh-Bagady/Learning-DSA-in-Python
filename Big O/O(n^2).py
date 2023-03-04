d619339747cfe16d478cbbb69faf18f6f1be6a88
Manobal Singh Bagady <manobalsinghbagady.bt21ele@pec.edu.in>
2023-03-05 03:02:26 +0530

    O(n^2)

---
+++ Big O/O(n^2).py
@@ -0,0 +1,15 @@
+import sys
+sys.stdin = open("input.txt", "r")
+sys.stdout = open("output.txt", "w")
+sys.stderr = open("error.txt", "w")
+# Comment the above 4 lines if you are not using file input output.
+
+
+def print_items(n):
+    for i in range(n):
+        for j in range(n):
+            print(i, j)
+
+
+if __name__ == "__main__":
+    print_items(10)
