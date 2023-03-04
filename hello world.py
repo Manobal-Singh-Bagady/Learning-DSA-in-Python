9af269f0369349a7d2b6d7d7c61e3c75ed3d8d9b
Manobal Singh Bagady <manobalsinghbagady.bt21ele@pec.edu.in>
2023-03-05 02:27:50 +0530

    hello world

---
+++ hello world.py
@@ -0,0 +1,9 @@
+import sys
+sys.stdin = open("input.txt", "r")
+sys.stdout = open("output.txt", "w")
+sys.stderr = open("error.txt", "w")
+# Comment the above 4 lines if you are not using file input output.
+
+
+if __name__ == "__main__":
+    print("hello world")
