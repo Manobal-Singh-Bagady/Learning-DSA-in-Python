6224c8eff03bfba6cdbc7c3a131847de19604596
Manobal Singh Bagady <manobalsinghbagady.bt21ele@pec.edu.in>
2023-03-06 00:56:15 +0530

    Linked List Constructor

---
+++ Linked Lists/Constructor.py
@@ -0,0 +1,26 @@
+import sys
+try:
+    sys.stdin = open("input.txt", "r")
+    sys.stdout = open("output.txt", "w")
+    sys.stderr = open("error.txt", "w")
+except:
+    pass
+
+
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self, value):
+        newNode = Node(value)
+        self.head = newNode
+        self.tail = newNode
+        self.length = 1
+
+
+if __name__ == "__main__":
+    myLinkedList = LinkedList(4)
+    print(myLinkedList.head.value)
